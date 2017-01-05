1954085810f467459415393cc13aa876a3ea911f
javamv <javamv@gmail.com>
2017-01-05 10:39:51 +0200

    fixed paths

--- core/src/main/java/com/autowebinar/core/web/HelloController.java
+++ core/src/main/java/com/autowebinar/core/web/HelloController.java
@@ -14,53 +14,20 @@ import java.util.List;
 @RequestMapping()
 public class HelloController{
 
-    @RequestMapping(value="/hello", method = RequestMethod.GET)
+    @RequestMapping(value="/api/hello", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
     }
 
-    @RequestMapping(value="/login", method = RequestMethod.GET)
+    @RequestMapping(value="/api/login", method = RequestMethod.GET)
     public String login(ModelMap model) {
         model.addAttribute("message", "Добро Пожаловать!");
         model.addAttribute("signed", "false");
         return "login";
     }
 
-    @RequestMapping(value="/vk_auth", method = RequestMethod.GET)
-    public String vkAuth(ModelMap model, HttpServletRequest request) {
-        model.addAttribute("message", request.getParameter("name")+", �� ������ ������� �����������!");
-        model.addAttribute("signed", "true");
-        model.addAttribute("id", request.getParameter("name"));
-        model.addAttribute("image", request.getParameter("url"));
-        return "login";
-    }
-
-    @RequestMapping(value="/calculate_credit", method = RequestMethod.GET)
-    public String acceptCreditDetails(Model model, HttpServletRequest request) {
-        model.addAttribute("message", "Our model is:"+request.getParameter("body"));
-        model.addAttribute("calculation", "true");
-        request.setAttribute("calculation", "true");
-
-        int numMonths = Integer.parseInt(request.getParameter("months"));
-        int body = Integer.parseInt(request.getParameter("body"));
-        int payRate = Integer.parseInt(request.getParameter("rate"));
-
-        int payBody = body / numMonths;
-        int repayRate = payBody * payRate / 100;
-
-        List<Credit> creditList = new ArrayList<Credit>(numMonths);
-
-        for (int i=0; i < numMonths; i++) {
-            creditList.add(new Credit(i+1, payBody, repayRate));
-        }
-
-        request.setAttribute("credit", creditList);
-
-        return "hello";
-    }
-
-    @RequestMapping(value="/add_cat", method = RequestMethod.GET)
+    @RequestMapping(value="/api/add_cat", method = RequestMethod.GET)
     public String addCat(Model model, HttpServletRequest request) {
         request.setAttribute("vasinCat", "true");
         return "hello";
